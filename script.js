a4431d48ade2b6a48e975e4a74d361468a8338d9
viveksingh3602 <149920917+viveksingh3602@users.noreply.github.com>
2024-08-27 10:17:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on("click", function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
+        } // End if
+    });
+});
+
+
+
+
+
+
+
+
+/*document.querySelector('form').addEventListener('submit', function(event) {
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const message = document.querySelector('#message').value.trim();
+
+    if (!name || !email || !message) {
+        alert('Please fill in all fields.');
+        event.preventDefault();
+    } else {
+        alert('Thank you for your message!');
+    }
+});*/
